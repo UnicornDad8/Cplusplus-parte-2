cc13aab90d323d45e26b62dde2a0f1c979d1d062
UnicornDad8 <unicorndad8@gmail.com>
2024-10-23 06:24:34 -0300

    Agregar ejemplo de raw string

---
+++ RawStrings/main.cpp
@@ -0,0 +1,16 @@
+// C++ (intermedio) - Strings
+// ---------------------------------------------------------
+// Raw strings
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    string str = R"("c:\folder_1\folder_2\file.txt")";
+    
+    cout << str << endl;
+   
+    return 0;
+}
