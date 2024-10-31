bfc098c17b2783762dd51d366f6195c844875c80
UnicornDad8 <unicorndad8@gmail.com>
2024-10-31 10:21:11 -0300

    Mejorando implementacion del ejercicio de la clase anterior

--- OperatorOverloadingExercise/main.cpp
+++ OperatorOverloadingExercise/main.cpp
@@ -1,27 +1,28 @@
 // C++ (intermedio) - Strutures and Enumerations
-// -----------------------------------------------------------------------------------------------
+// ------------------------------------------------------------
 // Sobreacarga de operadores.
-// -----------------------------------------------------------------------------------------------
+// ------------------------------------------------------------
 // Defina una estructura para representar un punto.
 // - x
 // - y
 //
-// Sobrecargue los operadores de igualdad (==) e inserción de flujo (<<) para los puntos.
-// imprima el punto 1 y el punto dos seguido de un mensaje de igualdad si son iguales o de
-// diferencia si son diferentes.
+// Sobrecargue los operadores de igualdad (==) e 
+// inserción de flujo (<<) para los puntos.
+// imprima el punto 1 y el punto 2 seguido de un 
+// mensaje de igualdad si son iguales o de diferencia 
+// si son diferentes.
 
 #include <iostream>
 
 using namespace std;
-
 
 struct Punto {
     int x = 0;
     int y = 0;
 };
 
-bool operator==(const Punto& primero, const Punto& segundo) {
-    return (primero.x == segundo.x && primero.y == segundo.y);
+bool operator==(const Punto& primer, const Punto& segundo) {
+    return (primer.x == segundo.x && primer.y == segundo.y);
 }
 
 ostream& operator<<(ostream& stream, const Punto& punto) {
@@ -32,14 +33,14 @@ int main() {
     Punto punto1 = { 5, 7 };
     Punto punto2 = { 5, 7 };
     
-    cout << punto1 << "\t\t" << punto2 << endl;
+    cout << punto1 << " " << punto2 << endl;
+    cout << "\n";
     
-    cout << "\n\n";
+    string mensaje;
+    (punto1 == punto2) ? mensaje = "iguales" 
+    : mensaje = "Diferentes";
     
-    if(punto1 == punto2)
-        cout << "Iguales" << endl;
-    else
-        cout << "Diferentes" << endl;
-    
+    cout << mensaje << endl;
+
     return 0;
 }
