fdbb08f5633ce3839af8b245d9156edf29ea2b2d
UnicornDad8 <unicorndad8@gmail.com>
2024-11-07 10:48:38 -0300

    Ejercicio: Parsear un string y convertirla en una estructura

--- ParsingStrings/main.cpp
+++ ParsingStrings/main.cpp
@@ -1,25 +1,39 @@
 // C++ (intermedio) - Streams and Files
 // -----------------------------------------------------------------------------------------------
 // Parseando cadenas de texto o "Parsing strings".
+// -----------------------------------------------------------------------------------------------
+// Ejercicio:
+// Dada la siguiente cadena de texto, escribe una función para parsear y convertirla
+// en una estructura de película (struct Movie).
+//
+// "Terminator 1,1984"
 
 #include <iostream>
 #include <sstream>
 
 using namespace std;
 
-int main() {
-    string str = "10 20";
+struct Movie {
+    string title;
+    int year;
+};
+
+Movie parseMovie(string str) {
     stringstream stream;
-    
     stream.str(str);
     
-    int first;
-    stream >> first;
+    Movie movie;
+    getline(stream, movie.title, ',');
+    stream >> movie.year;
     
-    int second;
-    stream >> second;
+    return movie;
+}
+
+int main() {
+    Movie movie = parseMovie("Terminator 1,1984");
     
-    cout << first + second << endl;
+    cout << "Title: " << movie.title << endl;
+    cout << "Year: " << movie.year << endl;
     
     return 0;
 }
